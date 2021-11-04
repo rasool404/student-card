bb5b57b4aacb1df616dc62ca5965edae189f4176
Rasul <rasuljonhalimov47@gmail.com>
2021-11-04 14:36:38 +0500

    change regex for url

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,49 +1,54 @@
 const validator = (data, config) => {
-    const errors = {};
+  const errors = {};
 
-    const validate = (validateMethod, data, config) => {
-        switch (validateMethod) {
-            case 'isRequired':
-                if (!data.trim()) return config.message;
-                break;
-            case 'isValidName': {
-                const length = data.trim().length;
-                if (length < 3 || length > 20) return config.message;
-                break;
-            }
-            case 'isURL': {
-                const urlRegExp = /^http(s)?:\/\/\S+\.[a-z]+(\/)?$/g;
-                if (!urlRegExp.test(data)) return config.message;
-                break;
-            }
-            case 'isValidYear': {
-                if (
-                    Number(data) < 1940 ||
-                    Number(data) > new Date(Date.now()).getFullYear()
-                )
-                    return config.message;
-                break;
-            }
-            default:
-                break;
+  const validate = (validateMethod, data, config) => {
+    switch (validateMethod) {
+      case "isRequired":
+        if (!data.trim()) return config.message;
+        break;
+      case "isValidName": {
+        const length = data.trim().length;
+        if (length < 3 || length > 20) return config.message;
+        break;
+      }
+      case "isURL": {
+        const expression =
+          /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+        const regex = new RegExp(expression);
+
+        if (!data.match(regex)) {
+          return config.message;
         }
-    };
-
-    for (const fieldName in data) {
-        for (const validateMethod in config[fieldName]) {
-            const error = validate(
-                validateMethod,
-                data[fieldName],
-                config[fieldName][validateMethod]
-            );
-
-            if (error && !errors[fieldName]) {
-                errors[fieldName] = error;
-            }
-        }
+        break;
+      }
+      case "isValidYear": {
+        if (
+          Number(data) < 1940 ||
+          Number(data) > new Date(Date.now()).getFullYear()
+        )
+          return config.message;
+        break;
+      }
+      default:
+        break;
     }
+  };
 
-    return errors;
+  for (const fieldName in data) {
+    for (const validateMethod in config[fieldName]) {
+      const error = validate(
+        validateMethod,
+        data[fieldName],
+        config[fieldName][validateMethod]
+      );
+
+      if (error && !errors[fieldName]) {
+        errors[fieldName] = error;
+      }
+    }
+  }
+
+  return errors;
 };
 
 export default validator;
